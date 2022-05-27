a4d06245391f1c3b23ae72171154dd3ee3379a5b
mohamedawad01 <mohamedawad2804@gmail.com>
2022-05-27 20:32:37 +0200

    first int

---
+++ queries.sql
@@ -0,0 +1,33 @@
+-- --------------- 1 ---------------
+SELECT orderNumber,  SUM(priceEach*quantityOrdered) AS totalAmount FROM `orderdetails`
+GROUP by orderNumber;
+
+
+-- --------------- 2 ---------------
+SELECT concat(emp.firstName," ", emp.lastName) AS empFullname, 
+concat(mang.firstName," ", mang.lastName)AS mangFullname 
+FROM employees as emp JOIN employees as mang 
+ON emp.reportsTo = mang.employeeNumber;
+
+
+
+-- --------------- 3 ---------------
+SELECT concat(firstName," ",lastName)AS fullName FROM `employees` WHERE  officeCode
+IN (SELECT officeCode FROM `offices` WHERE city = 'london');
+
+
+
+-- --------------- 4 ---------------
+SELECT  concat(employees.firstName," ",employees.lastName)AS fullName ,
+offices.city 
+FROM employees 
+JOIN offices
+ON employees.officeCode = offices.officeCode  
+WHERE city = 'london';
+
+
+-- --------------- 5 ---------------
+SELECT customers.customerName, SUM(priceEach*quantityOrdered)AS totalAmount FROM `customers` JOIN orders JOIN orderdetails
+ON customers.customerNumber = orders.customerNumber
+AND orders.orderNumber = orderdetails.orderNumber
+GROUP BY customers.customerName;
